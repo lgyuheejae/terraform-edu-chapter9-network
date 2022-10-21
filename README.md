0dd298c2e68991c6b547a01f381df28b40f21da8
yuheejae <yuheejae@lguplus.co.kr>
2022-10-21 11:50:59 +0900

    init source

---
+++ README.md
@@ -0,0 +1,9 @@
+# Chapter 7. Workflow - Terraform Cloud
+A terraform built application for use in Terraform Education program
+
+센티넬 적용
+- 태깅, 프리픽스 검토
+- git 협업체계... 도입
+
+협업의 방식
+- 로컬에서 최대한 테스트하자 (매뉴얼/가이드)
---
+++ outputs.tf
@@ -0,0 +1,12 @@
+#output "catapp_url" {
+#  value = {
+#    for instance in aws_instance.hashicat : instance.id => "http://${instance.public_dns}"
+#  }
+#}
+#
+#output "catapp_ip" {
+#  value = {
+#    for instance in aws_instance.hashicat : instance.id => "http://${instance.public_ip}"
+#  }
+#
+#}
